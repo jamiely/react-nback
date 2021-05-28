acfe03578d62c4738dd5949912fdd6e383654b2d
Jamie Ly <jamie.ly@gmail.com>
2021-05-27 22:51:51 -0400

    Fixes Game tests broken due to history changes

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,4 +1,4 @@
-import { createGame, isMatch, newRound, Game } from './Game';
+import { createGame, isMatch, newRound, Game, MatchState } from './Game';
 
 const r0 = createGame();
 const r1 = newRound(r0);
@@ -11,17 +11,20 @@ const manufacturedNonMatch: Game = {
   ...r2,
   current: {
     ...r0.current,
-    location: {
-      row: -1, column: -1
-    }
+    symbol: {
+      ...r0.current.symbol,
+      location: {
+        row: -1, column: -1
+      }
+    },
   }
 };
 
 test('game structure should match', () => {
-  expect(r2.symbols.length).toBe(2);
+  expect(r2.history.length).toBe(2);
   expect(r2.roundsBack).toBe(2);
-  expect(r2.symbols).toEqual([r1.current, r0.current])
-  expect(r1.current).toBe(r2.symbols[0]);
+  expect(r2.history).toEqual([r1.current, r0.current])
+  expect(r1.current).toBe(r2.history[0]);
 });
 
 test('matching should work', () => {
